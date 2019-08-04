867b1d1dfb3e4332a9e517aac3e837215cb5b9b5
Manish <manish.chandwaney@gmail.com>
2019-08-04 17:00:48 -0400

    prod devtool, bundle.js config

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/RandomizerApp';
+import RandomizerApp from './components/RandomizerApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
