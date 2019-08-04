d2723508b49221fbf860c1fb9a443feb6bec9894
Manish <manish.chandwaney@gmail.com>
2019-08-04 17:10:46 -0400

    webpack.config devServer change, babelrc presets change

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         mode: 'none',
@@ -29,7 +29,8 @@ module.exports = (env) => {
         },
         devtool: isProduction? 'source-map' : 'inline-source-map',
         devServer: {
-            contentBase: path.join(__dirname, 'public')
+            contentBase: path.join(__dirname, 'public'),
+            publicPath: '/dist/'
         }
     }
 };
