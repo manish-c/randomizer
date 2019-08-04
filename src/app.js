5b920f152d4d19b36225b04baed7f6401fdd33ff
Manish <manish.chandwaney@gmail.com>
2019-08-04 16:24:51 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/RandomizerApp';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<RandomizerApp />, document.getElementById('app'));
+
+ 
